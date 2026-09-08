082ddfaf9b9d9fc5b95131097cc0ecf00e7b343a
Reza <rezaghezelseflou@gmail.com>
2026-09-08 22:58:06 +0330

    suspensed study page

--- app/study/page.tsx
+++ app/study/page.tsx
@@ -1,13 +1,19 @@
 // /app/study/page.tsx
 import StudyTabs from "@/components/study-page/study-tabs";
 import { BookProvider } from "@/components/study-page/book/book-provider";
+import { Suspense } from "react";
 
 const StudyPage = () => {
   return (
     <>
-      <BookProvider>
+      {/* <BookProvider>
         <StudyTabs />
-      </BookProvider>
+      </BookProvider> */}
+      <Suspense fallback={null}>
+        <BookProvider>
+          <StudyTabs />
+        </BookProvider>
+      </Suspense>
     </>
   );
 };
